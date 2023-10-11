ccfcd2873f66504a4b8138072a3387deaf43170d
kristopher <007751114@coyote.csusb.edu>
2023-10-11 01:25:12 -0700

    Oct 11 push

---
+++ tello_test_run.py
@@ -0,0 +1,152 @@
+from djitellopy import Tello
+import time,keyboard
+import pandas as pd
+import schedule
+lockspeed,speed,rotdegrees=True,20,5
+trainingWheelsOff=False
+"""drone controls:
+    ascend/descend q/w
+    fwd/back "up"/"down"
+    strafe left/right "left"/"right"   
+    takeoff "9"
+    land "3"
+    flip "1"
+    toggle speed "space"
+
+"""
+while 1:
+    if trainingWheelsOff:
+        time.time()
+        schedule.every(1).seconds.do(drone_dynamics)
+        height=tello.get_height()
+        if tello.get_distance_tof()<40:
+            tello.move_back(20)
+    forward,backwards=keyboard.is_pressed('up'),keyboard.is_pressed('down')
+    rotLeft,rotRight=keyboard.is_pressed('a'),keyboard.is_pressed('d')
+    left,right=keyboard.is_pressed('left'),keyboard.is_pressed('right')
+    ascend,descend=keyboard.is_pressed('w'),keyboard.is_pressed('s')
+    speedToggle=keyboard.is_pressed('space')
+    
+    if speedToggle:
+        if lockspeed:
+            lockspeed=False          
+            speed=100    
+            rotdegrees=25       
+        else:
+            lockspeed=True
+            speed=20
+            rotdegrees=5
+        time.sleep(.15)# buffer needed to prevent rapid switching
+        if trainingWheelsOff:
+            tello.set_speed(speed)
+        
+    
+    flip=keyboard.is_pressed('1')
+    takeoff=keyboard.is_pressed('9')
+    land=keyboard.is_pressed('3')
+    LR={'l':left,'r':right}
+    UD={'u':forward,'d':backwards}
+    user={'top':[ascend,left,rotLeft,takeoff,lockspeed],
+          'mid':[forward,flip,backwards,'XXXX',speedToggle],
+          'bot':[descend,right,rotRight,land,speed  ]}
+
+#    user={'top':[ascend,left,flip,takeoff,lockspeed],
+#          'mid':[forward,'XXXX',backwards,'XXXX',speedToggle],
+#          'bot':[descend,right,'XXXX',land,speed  ]}
+
+
+    #print('l:',left,'r:',right,'u:',forward,'d:',backwards,'flip:',flip)
+    if any([left , right , forward , backwards]):
+        
+        direction={'l':left,'r':right,'u':forward,'d':backwards}
+        first_true_direction = None
+        second_true_direction=None
+        for dir in direction:
+            if direction[dir] :
+                first_true_direction=dir
+                if first_true_direction in LR:
+                    second=UD
+                else:
+                    second=LR
+                break
+        
+        for dir in second:
+            if second[dir] and dir!=first_true_direction:
+                second_true_direction=dir
+                break
+        if flip :
+            if trainingWheelsOff:
+                tello.flip(first_true_direction)
+        else:
+            if trainingWheelsOff:
+                move.tello(first_true_direction,speed)
+                if second_true_direction!=None:
+                    move.tello(second_true_direction,speed)
+                if any(second_true_direction=='l' or first_true_direction=='l'):
+                    
+                    tello.rotate_counter_clockwise(rotdegrees)
+                elif any(second_true_direction=='r' or first_true_direction=='r'):
+                    tello.rotateclockwise(rotdegrees)
+            #else:
+            #    print('move in direction:',first_true_direction,second_true_direction,'@ speed',speed)
+    elif land:    
+        if trainingWheelsOff:
+            print('land_and_check()')#3
+        else:
+            print('landing drone')
+        break
+    elif takeoff:
+        if trainingWheelsOff:
+            tello.takeoff()
+        else:
+            print('tello.takeoff()')#9
+
+    df = pd.DataFrame(user)
+    #if trainingWheelsOff:
+    print(df,end="\033[F"*len(df))
+    
+tello = Tello()
+tello.connect()
+
+
+initial_state={ 'Battery': tello.get_battery(),
+                'Baro':tello.get_barometer(),
+                'Height':tello.get_height(),
+                'amb temp':tello.get_temperature()
+                                                                }
+print(f'Current State: {tello.get_current_state()}')
+print(f'Battery: {tello.get_battery()}%')
+print(tello.get_barometer())
+print(tello.get_highest_temperature(), tello.get_lowest_temperature())
+
+def drone_dynamics():
+    gyro={'pitch':tello.get_pitch(),
+        'roll':tello.get_roll(),
+        'yaw':tello.get_yaw(),
+        'height':tello.get_height()}
+    speed={'speed x':tello.get_speed_x(),
+        'speed y':tello.get_speed_y(),
+        'speed z':tello.get_speed_z()}
+    accel={'acceleration x':tello.get_acceleration_x(),
+        'acceleration y':tello.get_acceleration_y(),
+        'acceleration z':tello.get_acceleration_z()}
+    #location={'x dim':,'y dim':,'z dim':tello.get_height()}
+    missionDist={'x dim':tello.get_mission_pad_distance_x(),
+                 'y dim':tello.get_mission_pad_distance_y(),
+                 'z dim':tello.get_mission_pad_distance_z()}
+    return gyro,speed,accel,missionDist
+
+def land_and_check():
+    height=tello.get_height()
+    tello.land()
+    drop_speed=tello.get_speed_z()
+    land_temp=tello.get_temperature()
+    droptime=height/drop_speed
+    time.sleep(droptime*1.05)
+    tello.turn_motor_on()
+    while land_temp>initial_state['amb temp']*1.15:
+        land_temp=tello.get_temperature()
+        time.sleep(5)
+    tello.turn_motor_off()
+    print('cooldown complete, current temp:',land_temp)
+
