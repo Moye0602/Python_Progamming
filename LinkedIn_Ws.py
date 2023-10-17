80bc9fb78ad1b5f2ca4b3140fc0ad5912ebbaa8e
kristopher <007751114@coyote.csusb.edu>
2023-10-17 13:18:53 -0700

    Web Scraping Example

--- LinkedIn_Ws.py
+++ LinkedIn_Ws.py
@@ -1,6 +1,10 @@
 import csv,json
 import sys,subprocess
 from Admin import*
+from icecream import *
+from pprint import *
+showDetails=0
+showSave=1
 while 1:
     print('loading dependencies')
     try:
@@ -21,12 +25,14 @@ while 1:
         sys.exit()
 
 # Extract and print the URLs
-#origin_source='https://www.linkedin.com/jobs/search/?currentJobId=3446845024&distance=25.0&geoId=103644278&keywords=cyber%20security&origin=HISTORY'
-
-#origin_source='https://www.linkedin.com/jobs/view/cyber-security-analyst-at-experfy-3646115215?refId=jqecaA576yTWX0d5TKepkQ%3D%3D&trackingId=fqzb%2B26mnHInp%2BgcBQbb%2Fg%3D%3D&position=25&pageNum=0&trk=public_jobs_jserp-result_search-card'
-#origin_source='https://www.linkedin.com/jobs/search/?currentJobId=3736023585&keywords=cybersecurity&origin=BLENDED_SEARCH_RESULT_CARD_NAVIGATION'
 fileName='LinkedIn'
-origin_source=  'https://www.linkedin.com/jobs/cyber-security-jobs'
+page=25
+#origin_source=  'https://www.linkedin.com/jobs/cyber-security-jobs'
+    # this link does work but only allows parsing through a small portion of careers
+origin_source='https://www.linkedin.com/jobs/search/?currentJobId=3435698262&keywords=cyber%20security&start='
+    # this allows continious parsing of careers in incriments of 25 per main request
+
+
 
 def page_request(url):
     """ takes the first page from a redfin url and cycles through each home based on search criteria and returns a dictionary of information for each"""
@@ -43,153 +49,137 @@ def page_request(url):
         num+=1
         jobs[num]={'Link':i.get('href')}
     return jobs
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+base_dict={ 'Role':None,'Link':None,'Salary':None,'datePosted':None,
+                'postExperiation':None,'educationReqs':None,
+                'experienceReqs':None,'skillReqs':None,'employemenType':None,           
+                'Industry':None,'Location':None}    
+# base_dict is a template of information for each job
+details={
+            'Role':'title',
+            'Link':'link',
+            'Salary':'baseSalary',
+            'datePosted':'datePosted',
+            'postExperiation':'validThrough',
+            'educationReqs':'educationRequirements',
+            'experienceReqs':'experienceRequirements',
+            'skillReqs':'skills',
+            'employemenType':'employmentType',
+            'Industry':'industry',
+            'Location':'jobLocation'
+            }
+#details is a local dictionary used for trasnlating the information collected from LinkedIn to our dictionary
+
+
+#the first is creates a blank excel csv file with field names based on the keys from the dictionary "details"
+with open('LinkedIn.csv','w') as iWannaWorkcsv:
+        csv_writer = csv.DictWriter(iWannaWorkcsv, fieldnames = list(details.keys()) )
+        csv_writer.writeheader()
+        crayon('New blank csv file created','green')
+#the second is creating a blank python file that will be appended to 
+# we can put anything here that is python readable
+with open('LinkedIn_jobs.py','w') as iWannaWork:
+    crayon('New blank python file created','green')
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 try:
+    page,pageMax=25,8
+    id=0
     job={}
-    jobs = page_request(origin_source)
-    #print(jobs)
-    for num in jobs:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(requests.get(jobs[num]['Link']).content, "html.parser")
-        #print(soup.find_all())
-        elements= soup.find_all('script', type='application/ld+json')[0].text
-        num=1
-#        temp=dict()
-        elements.split('":')
-        elements=json.loads(elements)
-        
-            #json used to convert the string output to a dictionary
-        #pprint(elements)
-        job[num]={ 'Role':elements['title'],
-                    'Link':jobs[num]['Link'],
-                    'Salary':{'max': elements['baseSalary']['value']['maxValue'],'min': elements['baseSalary']['value']['minValue']},
-                   'datePosted':elements['datePosted'],
-                   'postExperiation':elements['validThrough'],
-                    'educationReqs':elements['educationRequirements']['credentialCategory'],
-                    'experienceReqs':elements['experienceRequirements']['monthsOfExperience'],
-                    'skillReqs':elements['skills'],
-                    'employemenType':elements['employmentType'],
+    for i in range(1,pageMax):
+        #for pages one to the value of pageMax, do the following instructions
+        print('\n','Page:',i,'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+        jobs = page_request(origin_source+str(page*i))
+        #request all html data from the link found
+        if showDetails:
+            ic(jobs)
+        for num in jobs:
+            #for all jobs found in our request, search the HTML content using BeautifulSoup
+            soup = BeautifulSoup(requests.get(jobs[num]['Link']).content, "html.parser")
+            try:
+                elements= soup.find_all('script', type='application/ld+json')
+                #find anything matching the tag"script" and data type "'application/ld+json'"
+                if len(elements)>0:
+                    #if we find a list of things then we keep going
+                    if showDetails:
+                        print(elements)
+                    # what we do find, needs to be converted to something useful for both python and us to read
+                    elements=elements[0].text
+                        #text conversion
+                    elements.split('":')
+                        #split at the ' ": '
+                    elements=json.loads(elements)
+                    #json used to convert the string output to a dictionary
+
+                    job[id]=base_dict 
+                        #copy over our blank state dictionary
+                    for name in details:
+                        #for each name inour dictionary translation "details", do the following
+                        category=details[name]
+                            # not neeeded but I create local variables to reduce typing
+                        if category in elements:
+                            #check wht the category is and that it's in elements, sometimes there are blanks
+                            if category=='jobLocation':
+                                job[id][name]={'State':elements['jobLocation']['address']['addressRegion'],'City':elements['jobLocation']['address']['addressLocality']}
+                            elif category=='baseSalary':
+                                job[id][name]={'max':elements['baseSalary']['value']['maxValue'],'min': elements['baseSalary']['value']['minValue']}
+                            elif category=='educationRequirements' :
+                                job[id][name]=elements['educationRequirements']['credentialCategory']
+                            elif category=='experienceRequirements':
+                                job[id][name]=elements['experienceRequirements']['monthsOfExperience']    
+                            else:
+                                job[id][name]=elements[category]
+                            if showDetails:
+                                ic(category)
+                        if category=='link':
+                            job[id][name]=jobs[num]['Link']
+                            # this last category is an exception because it was created outside
+                            #  of the information we called, but we still need the link of the job
+                    if showDetails:
+                        crayon([id,job[id]['Role']])
+                        pprint(job[id])
+                        print('\n')
+            except Exception as error:
+                #errors can still occur and we want to know what they are so
+                pprint(elements)
+                    ##pprint to show our request gave
+                print('<<<>>>')
+                ic(elements)
+                    #ic to idenitfy a bit easier
+                crayon(error,'red')
+                    #error code of what actually happened
+                timeout(10)
+                    #time to jump in OR ignore and let the program continue
+            if id in job:
+                #now that we got the info, we can store it in a csv file with the following
+                with open('LinkedIn.csv','a') as iWannaWorkcsv:
+                    if showDetails:
+                        ic(list(details.keys()))    
+                    csv_writer = csv.DictWriter(iWannaWorkcsv, fieldnames = list(details.keys()) )
                     
-                    'Industry':elements['industry'],
-                    'Location':{'State':elements['jobLocation']['address']['addressRegion'],'City':elements['jobLocation']['address']['addressLocality']},
-        }
-        #print(job)
-        #timeout(1000)
-#except ConnectionAbortedError:#
+                    
+                    csv_writer.writerow(job[id])
+            blank()
+            print('ID',id, 'of',pageMax*25,'stored')
+            num+=1
+            id+=1
+            
+            #iteration to the next id number and job from the request of 25 made earlier
+            
+        # writing the python file is a different in that we are not writing in one line at a time like csv 
+        #instead, after every 25 jobs or page completion, we update the file with the changes and a timestamp
+        with open('LinkedIn_jobs.py','w') as iWannaWork:
+            iWannaWork.write('Jobs='+str(job)+'\n'+'timeStamp="'+str(datetime.now())+'"')
+        if showSave:
+            ic(iWannaWork)
+            print()
+            ic(iWannaWorkcsv)
+except KeyboardInterrupt:
+    restart_file()
 except Exception as error:
     crayon(error,'red')
-    timeout(9000)
-    restart_file()
-finally: 
     restart_file()
 restart_file()
-page=0
-reqs=0
-id=0
-prev_link=None
-with open(fileName+'.csv', 'w') as Redfin:
-    csv_writer = csv.DictWriter(Redfin, fieldnames = jobs.keys() )
-    csv_writer.writeheader()
-
-checks=2000
-if 0:
-    while checks>0:
-        print('\n','Page:',page,'<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-        try:
-    #list of all homes by page number
-            response = requests.get(source)
-            soup = BeautifulSoup(response.content, "html.parser")
-            link_elements = soup.find_all('a')
-            for link in link_elements:
-    #link to the individual page        
-                link_url = link.get('href')
-                if prev_link!=link_url and link_url and '/home/' in link_url:  # Check if the link has an href attribute
-                    id+=1
-                    print(id,link_url)
-                    url=link_url
-                    home=page_request(url)
-                    allHomes[home['Address']]=home
-
-                    #allHomes[id]=home #alternate that uses the id number rather than home address
-                    #print(allHomes) 
-                    print('>'*4,home['Address'],home)
-                    print('///')
-                    prev_link=link_url
-                    with open('Redfin_VA.csv','a') as Redfin:
-                        csv_writer = csv.DictWriter(Redfin, fieldnames = fieldnames )
-                        try:
-                            price=int(home['HomeFacts']['List Price'].replace("$", "").replace(",", ""))
-                        except:
-                            print(home['HomeFacts'])
-                            user=input('push a key to continue')
-                        sqft=0
-                        try:
-                            sqft=int(home['Sq Ft'].replace(",", ""))
-                        except:
-                            pass
-                        store['id']=id
-                        store['Property Type']=home['HomeFacts']['Property Type']
-                        store['Address']=home['Address']
-                        store['City']=home['City']
-                        store['State']=home['State']
-                        store['Zip Code']=home['Zip Code']
-                        store['Price']=price
-                        store['Beds']=home['Beds']
-                        store['Baths']=home['Baths']
-                        store['Sq Ft']=sqft
-                        try:
-                            store['Location']=home['HomeFacts']['Community']
-                        except KeyError:
-                            store['Location']=None
-                        try:                    
-                            store['Lot Size']=int(home['HomeFacts']['Lot Size'].replace(",", "").replace(" Sq. Ft.", ""))
-                        except ValueError:
-                            store['Lot Size']=home['HomeFacts']['Lot Size']
-                        except:
-                            store['Lot Size']=0
-                        store['Year Built']=home['HomeFacts']['Year Built']
-                        if 'hour' in home['HomeFacts']['Time on Redfin']:
-                            store['Days on Market']=0
-                        else:
-                            store['Days on Market']=home['HomeFacts']['Time on Redfin'].replace('days',"")
-                        store['$/Square Feet']=round(price/sqft,2)
-                        try:
-                            store['HOA / Month']=home['HomeFacts']['HOA Dues'].replace('$',"").replace('/month','')
-                        except:
-                            store['HOA / Month']=0
-                        store['URL']=url
-                        #parsedHomes[store['Address']]=store
-                        
-                        print(store)
-                        print()
-                        csv_writer.writerow(store)
-                    checks=2000
-                else:
-                    checks-=1
-                    print('checks remaining:',checks,end='\r')
-
-            page+=1
-            source=origin_source+'/page-'+str(page)
-            
-                        
-                
-            
-        except KeyboardInterrupt:# to be changed upon hitting an error
-            print('User stopped process')
-            break
-        print('checks remaining:',checks)
-if 0:
-    parsedHomes={}
-    for title in allHomes[list(allHomes.keys())[0]]:
-        if title!='HomeFacts':
-            try:
-                parsedHomes[title]=[allHomes[address][title] for address in allHomes]
-            except:
-                parsedHomes[title]=[address for address in allHomes]
-    for address in allHomes:
-        title='HomeFacts'
-        for subtitle in allHomes[address][title]:
-            parsedHomes[subtitle]=[allHomes[address][title][subtitle] if subtitle in allHomes[address][title] else 0  for address in allHomes ]
-        
-            
-    dataframe=pd.DataFrame(parsedHomes)
-    print(dataframe)
