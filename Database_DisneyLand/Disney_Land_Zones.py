ac49ae3b8f29f3689ab5634a16dec41cc9afe543
Kris <007751114@coyote.csusb.edu>
2024-02-16 08:30:28 -0800

    2024 02 16 Theme park revisions, pre project day

--- Database_DisneyLand/Disney_Land_Zones.py
+++ Database_DisneyLand/Disney_Land_Zones.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Projects/Database_DisneyLand/Disney_Land_Zones.py
 zones = {
     "Fantasyland": {
         "attractions": ["Space Mountain", "Pirates of the Caribbean", "Haunted Mansion", "Splash Mountain"],
@@ -20,27 +19,4 @@ zones = {
         "diner": ["Main Street Bakery"],
         "shopping": ["Emporium", "Main Street Confectionery"]
     }
-=======
-zones = {
-    "Fantasyland": {
-        "attractions": ["Space Mountain", "Pirates of the Caribbean", "Haunted Mansion", "Splash Mountain"],
-        "diner": ["Cinderella's Royal Table", "Be Our Guest Restaurant"],
-        "shopping": ["Emporium", "Mickey's PhilharMagic Store"]
-    },
-    "Tomorrowland": {
-        "attractions": ["Buzz Lightyear Astro Blasters", "Star Tours", "Space Mountain"],
-        "diner": ["Auntie Gravity's Galactic Goodies"],
-        "shopping": ["Star Trader"]
-    },
-    "Adventureland": {
-        "attractions": ["Jungle Cruise", "Indiana Jones Adventure", "Pirates of the Caribbean"],
-        "diner": ["Tropical Hideaway"],
-        "shopping": ["Bengal Barbecue", "Indiana Jones Adventure Outpost"]
-    },
-    "Main Street": {
-        "attractions": ["Main Street Vehicles", "Walt Disney's Enchanted Tiki Room"],
-        "diner": ["Main Street Bakery"],
-        "shopping": ["Emporium", "Main Street Confectionery"]
-    }
->>>>>>> d5044abfad8f836a7d2f8b942446d6797e08c3b5:Database_DisneyLand/Disney_Land_Zones.py
 }
