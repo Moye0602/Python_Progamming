7b099ef5d01abc04eece5fb77d029a57bd5fc988
Moye0602 <104169119+Moye0602@users.noreply.github.com>
2024-02-15 18:43:28 -0800

    Delete stuff.py

--- stuff.py
+++
@@ -1,7 +0,0 @@
-import shutil
-path=__file__.split('/')
-path=('/').join(path[:-1])
-source_path =path+'stuff'+'.py'
-destination_path = path+'stuff'+'_dup.py'
-shutil.copy(source_path, destination_path)
-print('backup of ','stuff', 'complete')
